d3defdd10ebcacaf28f5948af11bd83529b9bc8d
Feliperaxd <lipeamaralsantos@gmail.com>
2025-07-29 22:02:53 -0300

    Dropdown selector code update

--- src/client/ui-structures/components/dropdown-selector/dropdown-selector.js
+++ src/client/ui-structures/components/dropdown-selector/dropdown-selector.js
@@ -23,6 +23,7 @@ export default class DropdownSelector {
     this.runningAnimation = false;
     this.relativeArrowPosition = null;
 
+    this.dom = this.#getDomRegistry();
     this.responsiveText = new ResponsiveText(
       'responsive-text',
       '--responsive-text-ratio',
@@ -35,11 +36,9 @@ export default class DropdownSelector {
 
   async init() {
     this.responsiveText.init();
-    /*
     this.#initElements();
     this.#bindEvents();
     this.relativeArrowPosition = this.#getRelativeArrowPosition();
-    */
   }
 
   #initElements() {
@@ -47,17 +46,158 @@ export default class DropdownSelector {
     [this.accentBar] = this.dom.getElements('accent-bar');
     [this.value] = this.dom.getElements('value');
     [this.arrow] = this.dom.getElements('arrow');
+    /*
     [this.menu] = this.dom.getElements('menu');
     this.menuItems = this.dom.getElements('menu-item');
     this.menuItemsValue = this.dom.getElements('menu-item-value');
+    */
+  }
+
+  #bindEvents() {
+    this.arrow.addEventListener('click', () => this.#openMenu());
+    this.value.addEventListener('click', () => { console.log('clickvalue'); });
+    window.addEventListener('click', e => {
+      if (!this.mainElement.contains(e.target)) {
+        this.#closeMenu();
+      }
+    });
+    this.mainElement.addEventListener('animationend', () => {
+      this.runningAnimation = false;
+    });
+  }
+
+  #setArrowProperties() {
+    this.arrow.style.setProperty(
+      '--arrow-top-default',
+      `${this.relativeArrowPosition.top}px`,
+    );
+    this.arrow.style.setProperty(
+      '--arrow-top-open',
+      `${this.#getRelativeMenuHeight()}px`,
+    );
+  }
+
+  #getRelativeArrowPosition() {
+    const arrowRect = this.arrow.getBoundingClientRect();
+    const parentRect = this.arrow.parentElement.getBoundingClientRect();
+    const top = arrowRect.top - parentRect.top;
+    const left = arrowRect.left - parentRect.left;
+    return { top, left };
+  }
+
+  #getRelativeMenuHeight() {
+    return 200;
+  }
+
+  #openMenu() {
+    if (this.runningAnimation) return;
+
+    this.runningAnimation = true;
+    this.#setArrowProperties();
+
+    switchStyleClass(
+      this.label,
+      this.dom.getSelector('label', false),
+      this.dom.getSelector('label-open', false),
+    );
+    switchStyleClass(
+      this.arrow,
+      this.dom.getSelector('arrow', false),
+      this.dom.getSelector('arrow-open', false),
+    );
+    switchStyleClass(
+      this.accentBar,
+      this.dom.getSelector('accent-bar', false),
+      this.dom.getSelector('accent-bar-open', false),
+    );
+    switchStyleClass(
+      this.value,
+      this.dom.getSelector('value', false),
+      this.dom.getSelector('value-open', false),
+    );
+
+    this.responsiveText.updateElements();
+    this.menuIsOpen = true;
+  }
+
+  #closeMenu() {
+    if (this.runningAnimation) return;
+
+    this.#setArrowProperties();
+
+    switchStyleClass(
+      this.label,
+      this.dom.getSelector('label-open', false),
+      this.dom.getSelector('label', false),
+    );
+    switchStyleClass(
+      this.arrow,
+      this.dom.getSelector('arrow-open', false),
+      this.dom.getSelector('arrow', false),
+    );
+    switchStyleClass(
+      this.accentBar,
+      this.dom.getSelector('accent-bar-open', false),
+      this.dom.getSelector('accent-bar', false),
+    );
+    switchStyleClass(
+      this.value,
+      this.dom.getSelector('value-open', false),
+      this.dom.getSelector('value', false),
+    );
+
+    this.responsiveText.updateElements();
+    this.menuIsOpen = false;
   }
 
   #getDomRegistry() {
     return new DomRegistry(
       [
+        /* -- Value -- */
+        {
+          selector: '.dropdown-selector__value--open',
+          name: 'value-open',
+          check: false,
+        },
         {
           selector: '.dropdown-selector__value--default',
           name: 'value',
+          check: false,
+        },
+
+        /* -- Arrow -- */
+        {
+          selector: '.dropdown-selector__arrow--open',
+          name: 'arrow-open',
+          check: false,
+        },
+        {
+          selector: '.dropdown-selector__arrow--default',
+          name: 'arrow',
+          check: false,
+        },
+
+        /* -- Label -- */
+        {
+          selector: '.dropdown-selector__label--open',
+          name: 'label-open',
+          check: false,
+        },
+        {
+          selector: '.dropdown-selector__label--default',
+          name: 'label',
+          check: false,
+        },
+
+        /* -- Accent bar -- */
+        {
+          selector: '.dropdown-selector__accent-bar--open',
+          name: 'accent-bar-open',
+          check: false,
+        },
+        {
+          selector: '.dropdown-selector__accent-bar--default',
+          name: 'accent-bar',
           check: false,
         },
       ],
